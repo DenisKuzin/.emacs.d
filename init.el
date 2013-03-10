1b74adaee3c0b34ff559ea171d890a6d5e906cb8
DenisKuzin <digiholiq@gmail.com>
2013-03-10 14:04:54 +0400

    clojure-mode

--- init.el
+++ init.el
@@ -2,3 +2,7 @@
 (epy-setup-checker "pyflakes %f")
 (setq skeleton-pair nil)
 (menu-bar-mode -1)
+(require 'package)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/"))
+(package-initialize)
